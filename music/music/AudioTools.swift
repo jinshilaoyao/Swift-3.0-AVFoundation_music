ec8d7341050e7d209a9076810a2a4bdfb1d2ea6f
jinshilaoyao <1145119186@qq.com>
2016-09-22 16:08:12 +0800

    no message

--- music/music/AudioTools.swift
+++ music/music/AudioTools.swift
@@ -7,7 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AudioTools: NSObject {
+    
+    var soundIDDict: [String: UInt32] = ["": 0]
+    
+    
+    class func playSystemSOundWith(url: URL) {
+        AudioServicesPlaySystemSound(AudioTools().loadSoundIDWith(url: url))
+    }
+    
+    class func playAlertSoundWith(url: URL) {
+        AudioServicesPlayAlertSound(AudioTools().loadSoundIDWith(url: url))
+    }
+    
+    fileprivate func loadSoundIDWith(url: URL) -> SystemSoundID {
+        
+        let urlString = url.absoluteString
+        
+        var soundID = soundIDDict[urlString] ?? 0
+        
+        if soundID == 0 {
+            AudioServicesCreateSystemSoundID(url as CFURL, &soundID)
+            soundIDDict[urlString] = soundID
+        }
+        
+        return soundID
+    }
+    
+    
+    class func clearMemory() {
+        
+    }
+    
+    
 
 }
--- music/music/ViewController.swift
+++ music/music/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        
+    }
+    
+    func labTwo() {
+        let url = Bundle.main.url(forResource: "我的滑板鞋.mp3", withExtension: nil)
+        
+        player = try? AVAudioPlayer(contentsOf: url!)
+        
+        player?.prepareToPlay()
+        
+        player?.play()
+        player?.delegate = self
+        
+//        player?.pause()
+        
+//        player?.stop()
+    }
+    
+    func labOne() {
+        
+//        最简单播放
+        
+        let url = Bundle.main.url(forResource: "m_03.wav", withExtension: nil)
+        
+        AudioTools.playSystemSOundWith(url: url!)
+        
+        AudioTools.playAlertSoundWith(url: url!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+extension ViewController {
+    
+}
+
 
