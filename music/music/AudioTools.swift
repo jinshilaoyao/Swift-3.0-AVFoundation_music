0c6091497fc64d2105298dd49de06c617703f753
jinshilaoyao <1145119186@qq.com>
2016-09-22 16:08:06 +0800

    no message

---
+++ music/music/AudioTools.swift
@@ -0,0 +1,13 @@
+//
+//  AudioTools.swift
+//  music
+//
+//  Created by yesway on 16/9/22.
+//  Copyright © 2016年 joker. All rights reserved.
+//
+
+import UIKit
+
+class AudioTools: NSObject {
+
+}
