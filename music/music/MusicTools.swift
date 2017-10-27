2c3faf552acd2f810123a1d2e399a4e16fd235c4
yaoyao <595133914@qq.com>
2017-10-27 09:49:40 +0800

    add tool

---
+++ music/music/MusicTools.swift
@@ -0,0 +1,13 @@
+//
+//  MusicTools.swift
+//  music
+//
+//  Created by yaoyao on 2017/10/27.
+//  Copyright © 2017年 joker. All rights reserved.
+//
+
+import UIKit
+
+class MusicTools: NSObject {
+    let name: String = "am i that easy to forget"
+}
